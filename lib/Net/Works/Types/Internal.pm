f5e4db2a8aec00dcc778c0d4e98d549620e294e5
Dave Rolsky <autarch@urth.org>
2013-01-21 16:59:06 -0600

    Really fix the constraint inlining

--- lib/Net/Works/Types/Internal.pm
+++ lib/Net/Works/Types/Internal.pm
@@ -23,7 +23,8 @@ subtype PackedBinary,
 
 subtype IPInt,
     as Int | UInt128,
-    inline_as {  "$_[0] > 0" },
+    where { $_ > 0 },
+    inline_as { $_[0]->parent()->_inline_check( $_[1] ) . " && ($_[1] > 0)" },
     message { ( defined $_ ? $_ : 'undef' ) . ' is not a valid IP integer' };
 
 subtype IPVersion,
@@ -31,7 +32,11 @@ subtype IPVersion,
 
 subtype MaskLength,
     as Int,
-    inline_as { "$_ >= 0 && $_ <= 128" },
+    where { $_ >= 0 && $_ <= 128 },
+    inline_as {
+        $_[0]->parent()->_inline_check( $_[1] )
+            . " && ($_[1] >= 0 && $_[1] <= 128)";
+    },
     message {
         ( defined $_ ? $_ : 'undef' ) . ' is not a valid IP mask length';
     };
