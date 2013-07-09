631714dbfbdae8e3d97b3b06c64b8f090d8728b0
Dave Rolsky <autarch@urth.org>
2013-07-09 17:05:28 -0500

    Use Sub::Quote for all type check subs

--- lib/Net/Works/Types.pm
+++ lib/Net/Works/Types.pm
@@ -5,43 +5,50 @@ use warnings;
 
 use Exporter qw( import );
 use MooX::Types::MooseLike;
-use Scalar::Util qw( blessed );
+use Scalar::Util ();
+use Sub::Quote qw( quote_sub );
 
 my @types = (
     {
         name    => 'PackedBinary',
-        test    => sub { defined $_[0] && !ref $_[0] },
+        test    => quote_sub(q{ defined $_[0] && !ref $_[0] }),
         message => sub { _m( '%s is not binary data', $_[0] ) },
     },
     {
         name => 'IPInt',
-        test => sub {
+        test => quote_sub(
+            q{
             return 0 unless defined $_[0];
             return 1 if !ref $_[0] && $_[0] =~ /^[0-9]+$/;
             return 1
-                if blessed( $_[0] ) && $_[0]->isa('Math::UInt128');
+                if Scalar::Util::blessed( $_[0] ) && $_[0]->isa('Math::UInt128');
             return 0;
-        },
+        }
+        ),
         message => sub {
             _m( '%s is not a valid integer for an IP address', $_[0] );
         },
     },
     {
         name => 'IPVersion',
-        test => sub {
+        test => quote_sub(
+            q{
             defined $_[0]
                 && !ref $_[0]
                 && ( $_[0] == 4
                 || $_[0] == 6 );
-        },
+        }
+        ),
         message =>
             sub { _m( '%s is not a valid IP version (4 or 6)', $_[0] ) },
     },
     {
         name => 'MaskLength',
-        test => sub {
+        test => quote_sub(
+            q{
             !ref $_[0] && $_[0] >= 0 && $_[0] <= 128;
-        },
+        }
+        ),
         message => sub {
             _m(
                 '%s is not a valid IP network mask length (0-128)',
